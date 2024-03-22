55e7564e5c9cd7df5055df55b872094be4f41b52
sec0ndw1nd <phj98521@naver.com>
2024-03-22 20:32:37 +0900

    feat(s12-blog): render stylish code by using react syntax highlighter

--- s12-blog/src/components/posts/post-detail/PostContent.js
+++ s12-blog/src/components/posts/post-detail/PostContent.js
@@ -1,16 +1,61 @@
-import ReactMarkDown from 'react-markdown';
+import Image from 'next/image';
+import Markdown from 'react-markdown';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { atomDark } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 
 import PostHeader from './PostHeader';
 import styles from './PostContent.module.css';
 
 export default function PostContent({ post }) {
+  const customMarkdown = {
+    /* img(image) {
+      return (
+        <Image
+          src={`/images/posts/${post.slug}/${image.src}`}
+          alt={image.alt}
+          width={600}
+          height={300}
+        />
+      );
+    }, */
+
+    p({ node, children }) {
+      // override if it's an image in paragraph
+      if (node.children[0].tagName === 'img') {
+        const image = node.children[0];
+
+        return (
+          <div className={styles.image}>
+            <Image
+              src={`/images/posts/${post.slug}/${image.properties.src}`}
+              alt={image.properties.alt}
+              width={600}
+              height={300}
+            />
+          </div>
+        );
+      }
+
+      return <p>{children}</p>;
+    },
+
+    code(props) {
+      console.log(props);
+      return (
+        <SyntaxHighlighter style={atomDark} language="javascript">
+          {props.children}
+        </SyntaxHighlighter>
+      );
+    },
+  };
+
   return (
     <article className={styles.content}>
       <PostHeader
         title={post.title}
         image={`/images/posts/${post.slug}/${post.image}`}
       />
-      <ReactMarkDown>{post.content}</ReactMarkDown>
+      <Markdown components={customMarkdown}>{post.content}</Markdown>
     </article>
   );
 }
